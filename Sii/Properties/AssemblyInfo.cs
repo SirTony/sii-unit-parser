d8f45453a4c08b460b3f6b3d0089f7b36c50bf4c
Wilson212 <wilson.steven10@yahoo.com>
2016-09-24 14:58:38 -0500

    Fixed a couple files that got broken in the last 2 commits

--- Sii/Properties/AssemblyInfo.cs
+++ Sii/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sii File Parser for the SCS engine")]
-[assembly: AssemblyDescription("Sii File Parser for the SCS engine")]
+[assembly: AssemblyDescription("Sii File Parser for the SCS engine.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Plexis Software")]
 [assembly: AssemblyProduct("Sii File Parser")]
